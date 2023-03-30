c183a7b03bee61fce5fd68e29974cd65f4a12552
kachamozo <cachamocito@yahoo.com>
2023-03-30 17:44:38 -0400

    fix render deploy ssl required

--- api/src/db.js
+++ api/src/db.js
@@ -16,6 +16,11 @@ const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME, DATABASE_URL } =
 const sequelize = new Sequelize(DATABASE_URL, {
 	logging: false, // set to console.log to see the raw SQL queries
 	native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+	dialectOptions: {
+		ssl: {
+			require: true,
+		},
+	},
 });
 
 const basename = path.basename(__filename);
